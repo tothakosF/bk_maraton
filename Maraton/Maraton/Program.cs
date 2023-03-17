08ebf1f7e44422565180b037ae746dc4fe0da1f8
Toth Akos <toth.akos.ferenc24725@gmail.com>
2023-03-17 09:26:27 +0100

    files

---
+++ Maraton/Maraton/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maraton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> adatok = new List<Versenyzo>();
+
+            foreach (var item in File.ReadAllLines("Maraton.txt").Skip(1))
+            {
+                adatok.Add(new Versenyzo(item));
+            }
+
+            List<string> orszagok = new List<string>();
+
+            IDictionary<int, string> vesztesgeci = new Dictionary<int, string>();
+
+            foreach (var item in adatok)
+            {
+                if (!orszagok.Contains(item.Orszag))
+                {
+                    string currentString = item.Orszag;
+                    int currentInt = 0;
+                    orszagok.Add(currentString);
+                    foreach (var k in adatok)
+                    {
+                        if (k.Orszag == currentString)
+                        {
+                            currentInt++;
+                        }
+
+                        vesztesgeci.Add(currentInt, currentString);
+                        currentInt = 0;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Maraton/Maraton/Versenyzo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maraton
+{
+    internal class Versenyzo
+        {
+            public string Nev { get; set; }
+            public int Szulev { get; set; }
+            public string Kategoria { get; set; }
+            public string Orszag { get; set; }
+            public string Csapat { get; set; }
+            public string Varos { get; set; }
+            public string Rajtszam { get; set; }
+            public string Feltav { get; set; }
+            public string Eredmeny { get; set; }
+
+            public Versenyzo(string sor)
+            {
+                string[] resz = sor.Split(';');
+                Nev = resz[0];
+                Szulev = int.Parse(resz[1]);
+                Kategoria = resz[2];
+                Orszag = resz[3];
+                Csapat = resz[4];
+                Varos = resz[5];
+                Rajtszam = resz[6];
+                Feltav = resz[7];
+                Eredmeny = resz[8];
+            }
+        }
+}
